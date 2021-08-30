584efc3d35cc9a7b69be195aba1bcc0a9ee055ba
DmitrijPedan <dmitrijpedan84@gmail.com>
2021-08-30 16:46:01 +0300

    add animation

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Post from "./Post";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const PostList = ({title, posts, remove }) => {
   return (
     <div>
       <h1 style={{textAlign: 'center'}}>{posts.length ? title : 'Post not found'}</h1>
-      {posts.map((post, i) => <Post remove={remove} number={i + 1} post={post} key={post.id}></Post>)}
+      <TransitionGroup>
+        {posts.map((post, i) => {
+          return <CSSTransition
+            key={post.id}
+            timeout={500}
+            classNames="post"
+          >
+            <Post remove={remove} number={i + 1} post={post}/>
+          </CSSTransition>
+        })}
+      </TransitionGroup>
     </div>
   );
 };
